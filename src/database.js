b863013ade589f9cfdfcdddee74812b68936f58d
lebohang0824 <lebohang0824@gmail.com>
2020-05-04 10:25:43 +0200

    Visitors table method added

--- src/database.js
+++ src/database.js
@@ -5,7 +5,27 @@ const client = new Client();
 
 client.connect()
 
+const createVisitorsTable = async () => {
+
+	const sql = `
+		CREATE TABLE IF NOT EXISTS visitors (
+		    id SERIAL PRIMARY KEY,
+			    name varchar(255),
+			    age int,
+			    date_of_visit date,
+			    time_of_visit time,
+			    assistant varchar(255),
+			    comments text
+			);
+	`;
+
+	const res = await client.query(sql);
+
+}
+
 const addNewVisitor = async (visitor_name, visitor_age, date_of_visit, time_of_visit, assistant, comments) => {
+
+	await createVisitorsTable();
 
 	const sql = `
 		INSERT 
